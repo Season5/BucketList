2d651a6059ec40c5610891e6b576feebad4a2547
GraceJonn123 <gj.gjonn@gmail.com>
2016-03-08 14:51:36 +0300

    splash, manifest and intro

---
+++ app/src/main/java/com/jijjy/grace/thebucket/Main.java
@@ -0,0 +1,13 @@
+package com.jijjy.grace.thebucket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ app/src/main/java/com/jijjy/grace/thebucket/Splash.java
@@ -0,0 +1,17 @@
+package com.jijjy.grace.thebucket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by grace on 3/8/16.
+ */
+public class Splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+
+    }
+}
